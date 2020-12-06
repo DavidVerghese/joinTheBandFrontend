333537fd3889183b5ef3926900401e02850e6f77
David Verghese <dj.verghese@gmail.com>
2020-12-06 01:55:51 -0500

    saving it at current status

--- jointheband/src/App.js
+++ jointheband/src/App.js
@@ -44,7 +44,12 @@ function App() {
     <div className="App">
       <header><h1>Join the Band!</h1></header>
       <main>
+        <div class="intro">
+        <img src="https://media1.giphy.com/media/13QvN0u5j5SUj6/giphy.gif?cid=ecf05e47tsos9jw567r462i43oqpjho691ily093c8qurxiz&rid=giphy.gif"></img>
+        <em><h2>Tired of doing it all on your own?</h2></em>
+        </div>
         <div>
+          <h2>Profiles:</h2>
           {data.map((item) => {
             return (
               <div class="profile">
