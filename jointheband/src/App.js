529b18d66dacda7e6d96241d1ac22211c4fc199c
David Verghese <dj.verghese@gmail.com>
2020-12-05 23:47:05 -0500

    transferred over App.js from a different folder as I accidentally created a react app in the wrong folder

--- jointheband/src/App.js
+++ jointheband/src/App.js
@@ -1,9 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import { baseURL, config } from "./services";
+import { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
+  const [musician, setMusician] = useState('')
+  const [instrument, setInstrument] = useState('')
+  const [location, setLocation] = useState('')
+  const [genre, setGenre] = useState('')
+  const [lookingFor, setLookingFor] = useState('')
+  const [imageAddress, setImageAddress] = useState('')
+  const [data, setData] = useState([])
+  const fields = {
+    musician,
+    instrument,
+    location,
+    genre,
+    lookingFor,
+    imageAddress,
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(musician,instrument,lookingFor)
+  }
+
+  useEffect(() => {
+    async function getData() {
+      let response = await axios.get(baseURL + '/Musicians', config)
+      setData(response.data.records)
+      const req = await axios.post(baseURL + '/Musicians', { fields }, config)
+    }
+    getData();
+
+    // const handleSubmit = async (e) => {
+    //   e.preventDefault();
+    //   const fields = {
+    //     setup,
+    //     punchline,
+    //     author,
+    //   };
+    //   if (params.id) {
+    //     const jokeURL = `${baseURL}/${params.id}`;
+    //     await axios.put(baseURL, { fields }, config);
+    //   } else {
+    //     await axios.post(baseURL, { fields }, config);
+    //   }
+    //   props.setToggleFetch((prev) => !prev);
+    // };
+
+
+  }, [])
+
   return (
     <div className="App">
+      <header><h1>Join the Band!</h1></header>
+      <main>
+        <div>
+          {data.map((item) => {
+            return (
+              <div class="profile">
+                <h2>{item.fields.Musician}</h2>
+                <img src={item.fields.Picture}></img>
+                <p>Instrument: {item.fields.Instrument}</p>
+                <p>Genre: {item.fields.Genre}</p>
+                <p>Looking for: {item.fields.Looking_for}</p>
+                <p>Location: {item.fields.Location}</p>
+                <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/687495670&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div><a href="https://soundcloud.com/dabilliondollarbaby" title="Baby Jesus (DaBaby)" target="_blank" >Baby Jesus (DaBaby)</a> · <a href="https://soundcloud.com/dabilliondollarbaby/bop" title="BOP" target="_blank">BOP</a></div>
+              </div>
+            )
+          })}
+        </div>
+      </main>
+
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input name="name" type="text" onChange={(e) => setMusician(e.target.value)}/>
+      <label htmlFor="instrument">Instrument</label>
+      <input name="instrument" type="text" onChange={(e) => setInstrument(e.target.value)}/>
+      <label htmlFor="lookingFor">Looking For</label>
+      <input name="lookingFor" type="text" onChange={(e) => setLookingFor(e.target.value)}/>
+        <button>submit</button>
+        </form>
     </div>
   );
 }
