f3d8afc75ae798c75357c1f63e10789298823c4a
David Verghese <dj.verghese@gmail.com>
2020-12-07 09:58:39 -0500

    added in a search bar

--- jointheband/src/App.js
+++ jointheband/src/App.js
@@ -56,11 +56,19 @@ function App() {
 
   return (
     <div className="App">
-      <header><h1>Join the Band!</h1></header>
+      <header>
+        <h1>Join the Band!</h1>
+      <p>Home</p>
+      </header>
       <main>
         <div class="intro">
         <img src="https://media1.giphy.com/media/13QvN0u5j5SUj6/giphy.gif?cid=ecf05e47tsos9jw567r462i43oqpjho691ily093c8qurxiz&rid=giphy.gif"></img>
-        <em><h2>Tired of doing it all on your own?</h2></em>
+          <em><h2>Tired of doing it all on your own?</h2></em>
+          <label htmlFor="search">Search</label>
+          <input name="search" type="text" />
+          <button>Submit</button>
+          <button>View All Posts</button>
+          <button>Make a post!</button>
         </div>
         <div>
           <h2>Profiles:</h2>
