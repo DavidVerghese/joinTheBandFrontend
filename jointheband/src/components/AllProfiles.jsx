a1029fde5e0bef1832f78a6ddc06714ce158c6b3
David Verghese <dj.verghese@gmail.com>
2020-12-07 15:06:59 -0500

    created a new component Profile, that allows me to replace code in AllProfiles.jsx and Profile.jsx that was similar with a single component

--- jointheband/src/components/AllProfiles.jsx
+++ jointheband/src/components/AllProfiles.jsx
@@ -1,18 +1,14 @@
+import Profile from "./Profile.jsx"
+
 function AllProfiles(props) {
   return (
     <div>
       <h2>Profiles:</h2>
           {props.data.map((item) => {
             return (
-              <div class="profile">
-                <h2>{item.fields.Musician}</h2>
-                <img src={item.fields.Picture}></img>
-                <p>Instrument: {item.fields.Instrument}</p>
-                <p>Genre: {item.fields.Genre}</p>
-                <p>Looking for: {item.fields.Looking_for}</p>
-                <p>Location: {item.fields.Location}</p>
-                <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/687495670&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div><a href="https://soundcloud.com/dabilliondollarbaby" title="Baby Jesus (DaBaby)" target="_blank" >Baby Jesus (DaBaby)</a> · <a href="https://soundcloud.com/dabilliondollarbaby/bop" title="BOP" target="_blank">BOP</a></div>
-              </div>
+              <div>
+                <Profile item={item}/>
+                </div>
             )
           })}
     </div>
--- jointheband/src/components/SearchResults.jsx
+++ jointheband/src/components/SearchResults.jsx
@@ -1,4 +1,5 @@
 import "../App.css";
+import Profile from "./Profile.jsx";
 
 function SearchResults(props) {
   console.log("hey dude", props.musician);
@@ -7,12 +8,13 @@ function SearchResults(props) {
       {props.musician.map((item) => {
         return (
           <div>
-            <h2>{item.fields.Musician}</h2>
+            {/* <h2>{item.fields.Musician}</h2>
             <img src={item.fields.Picture}></img>
             <p>Instrument: {item.fields.Instrument}</p>
             <p>Genre: {item.fields.Genre}</p>
             <p>Looking for: {item.fields.Looking_for}</p>
-            <p>Location: {item.fields.Location}</p>
+            <p>Location: {item.fields.Location}</p> */}
+            <Profile item={item} />
           </div>
         );
       })}
