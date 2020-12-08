b8a0f82a0ac6c669314f4b55073ad783819b79dd
David Verghese <dj.verghese@gmail.com>
2020-12-08 12:13:52 -0500

    added a 'No Results Found' page if there are no serch results

--- jointheband/src/components/SearchResults.jsx
+++ jointheband/src/components/SearchResults.jsx
@@ -2,14 +2,23 @@ import "../App.css";
 import Profile from "./Profile.jsx";
 
 function SearchResults(props) {
-  return (
-    <div class="profile">
-      <div class="profilebackground">
-        {props.musician.map((item) => {
-          return (<Profile item={item} />)
-        })}
+  if (props.musician.length > 0) {
+    return (
+      <div class="profile">
+        <div class="profilebackground">
+          {props.musician.map((item) => {
+            return <Profile item={item} />;
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <h1>No Results Found!</h1>
+        <img src="https://media0.giphy.com/media/5x89XRx3sBZFC/giphy.gif" />
+      </div>
+    );
+  }
 }
 export default SearchResults;
