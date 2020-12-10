2315bca12a7b2656db3d2e024967ef80f31bedec
David Verghese <dj.verghese@gmail.com>
2020-12-09 23:38:33 -0500

    changed it so that the button can be hidden or displayed depednign on where you click

--- jointheband/src/components/SocialMedia.jsx
+++ jointheband/src/components/SocialMedia.jsx
@@ -1,10 +1,33 @@
 function SocialMedia(props) {
-  console.log(props.display);
-  return (
-    <div>
-      <button onClick={console.log("click!")}>Social Media/Contact Info</button>
-    </div>
-  );
+  if (props.todisplay) {
+    return (
+      <div class="socialMediaSection">
+        <div class="socialMedia">
+          <a href={props.item.fields.Facebook} target="_blank">
+            <img src="https://www.smartenergydecisions.com/upload/images/company_images/facebook.jpg" />
+          </a>
+        </div>
+        <div class="socialMedia">
+          <a href={props.item.fields.Soundcloud} target="_blank">
+            <img src="https://edm.com/.image/t_share/MTU5NDY5Nzk2NTUzOTI1OTA1/soundcloud.png" />
+          </a>
+        </div>
+        <div class="socialMedia">
+          <a href={props.item.fields.Instagram} target="_blank">
+            <img src="https://1000logos.net/wp-content/uploads/2017/02/Instagram-app-logo.jpg" />
+          </a>
+        </div>
+        <div class="socialMedia">
+          <a href={props.item.fields.Twitter} target="_blank">
+            <img src="https://www.creativefreedom.co.uk/wp-content/uploads/2017/06/Twitter-logo-2012.png" />
+          </a>
+        </div>
+      </div>
+    );
+  }
+  else {
+    return (<div></div>)
+  }
 }
 
 export default SocialMedia;
