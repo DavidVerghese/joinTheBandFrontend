e27bb7979744ef8ecd68b62959878a4fcd0cc1a2
David Verghese <dj.verghese@gmail.com>
2023-01-08 23:05:13 -0500

    unsuccessfully tried to use universal cookie to handle auth

--- jointheband/src/App.js
+++ jointheband/src/App.js
@@ -3,17 +3,36 @@ import { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home.jsx"
 import AllProfiles from "./components/AllProfiles.jsx"
-
+import Cookies from 'universal-cookie';
 import Login from './components/Login';
-import Signup  from './components/Signup';
+import Signup from './components/Signup';
+import Header from './components/Header';
+
 function App() {
   const [users, setUsers] = useState([]);
   
- 
-  console.log(process.env.NODE_ENV)
+  const cookies = new Cookies();
   const baseURL = process.env.NODE_ENV === 'production' ?  `https://join-the-band-api.herokuapp.com/` : `http://localhost:3000`
+  const [user, setUser] = useState(false);
 
-
+  console.log(user);
+  useEffect(() => {
+    const id = cookies.get('user_id');
+    fetch(`${baseURL}/lookup/${id}`)
+    .then(resp => {
+        if(resp.ok){
+          resp.json().then(data => {
+            setUser(data);
+              //  setUser(data)
+              //  data.error? setLoggedIn(false) : setLoggedIn(true)
+            })
+        }else {
+            // resp.json().then(data => setErrors(data.error))
+        }
+    })
+   
+  }, [])
+  
   useEffect(() => {
     fetch(`${baseURL}/users/`)
       .then((response) => response.json())
@@ -55,12 +74,12 @@ function App() {
   return (
     <div>
       <main>
-        
+        <Header user={user} />
       <Route exact path="/">
-          <Home/>
+          <Home user={user} />
         </Route>
         <Route path="/login">
-          <Login baseURL={baseURL} instruments={instruments} setInstruments={setInstruments} locations={locations} setLocations={setLocations} genres={genres} setGenres={setGenres} users={users} setUsers={setUsers} />
+          <Login user={user} setUser={setUser} baseURL={baseURL} instruments={instruments} setInstruments={setInstruments} locations={locations} setLocations={setLocations} genres={genres} setGenres={setGenres} users={users} setUsers={setUsers} />
         </Route>
 
         <Route path="/signup">
