ece8354f5273c9bda5239fd221c462b7ca44b715
David Verghese <dj.verghese@gmail.com>
2023-01-09 15:20:46 -0500

    styling to NoMatch

--- src/components/NoMatch/NoMatch.jsx
+++ src/components/NoMatch/NoMatch.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 function NoMatch() {
   return (<div className="no-match">
 
-          <Card style={{ backgroundColor: 'black',boxShadow: '1px 1px 1px 1px white',textAlign: 'center' }}>
+          <Card style={{ backgroundColor: 'black',boxShadow: '1px 1px 1px 1px white',textAlign: 'center', margin: '10px' }}>
           <Card.Body>
   <Card.Title><h1>ERROR 404!</h1></Card.Title>
   <Card.Text>
-  <Card.Text style={{color: 'red'}}>Oops</Card.Text> The page you're looking for does not exist
+  <Card.Text style={{color: 'red'}}><h2>Oops!</h2></Card.Text> The page you're looking for does not exist
     </Card.Text>
       <Link to="/"><Button style={{ margin: '0 auto' }} variant="primary">Home</Button></Link>
   </Card.Body>
